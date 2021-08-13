a32ba1de0fecb964ad8be22e6adafc93ee3b62af
65pnk <65pnk@cbr.ru>
2021-08-13 22:27:53 +0500

    fixes

---
+++ src/js/close.js
@@ -0,0 +1,19 @@
+import $ from "jquery";
+import { hideClass, showClass, shownClass } from "./showAnimations";
+
+$("[data-close]").each((_, el) => {
+  const element = $(el);
+  const targetSelector = element.data("close");
+  const target = $(targetSelector);
+
+  if (!target) {
+    console.error(`No element target element with selector ${target}`);
+    return;
+  }
+
+  element.on("click", () => {
+    target.removeClass(showClass);
+    target.addClass(hideClass);
+    target.removeClass(shownClass);
+  });
+});
