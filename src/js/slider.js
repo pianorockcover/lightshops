b9128b6ba05a8ea88f0e14e2e9124ca5bf1d07f7
65pnk <65pnk@cbr.ru>
2021-08-22 14:53:50 +0500

    fixes

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,26 +1,29 @@
-import SwiperCore, { Navigation, Pagination } from "swiper/core";
+import SwiperCore, {
+  Navigation,
+  Pagination
+} from "swiper/core";
 import Swiper from "swiper/bundle";
 
 SwiperCore.use([Navigation, Pagination]);
 
 const swiper = new Swiper("#mainSlider", {
-  // Optional parameters
   direction: "horizontal",
   loop: true,
   speed: 600,
 
-  // If we need pagination
+  autoplay: {
+    delay: 3000,
+  },
+
   pagination: {
     el: ".swiper-pagination",
   },
 
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 
-  // And if we need scrollbar
   scrollbar: {
     el: ".swiper-scrollbar",
   },
