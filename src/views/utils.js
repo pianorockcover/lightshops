1f0fa9f9e9272a1cc77c09a5393aaadfc2fbbd96
65pnk <65pnk@cbr.ru>
2021-10-01 16:54:56 +0500

    start making product card

--- src/views/utils.js
+++ src/views/utils.js
@@ -7,11 +7,23 @@ const randomElem = (items) => items[Math.floor(Math.random() * items.length)];
 
 const getUpload = (path) => `images/uploads/${path}`;
 
+const randomColor = () => {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+}
+
 module.exports = () => {
     return {
         randomInt,
         randomElem,
         faker,
         getUpload,
+        randomColor,
     }
 }
