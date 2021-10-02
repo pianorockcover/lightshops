409d9ba53dfa655fb0168b2c6de8b6890d2ec441
65pnk <65pnk@cbr.ru>
2021-10-02 16:03:00 +0500

    zoom view

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,4 @@ import "./toggle";
 import "./slider";
 import "./controls/contols";
 import "./tabs";
+import "./zoomView";
---
+++ src/js/zoomView.js
@@ -0,0 +1,14 @@
+import $ from "jquery";
+
+$("[data-zoom-view]").each((_, el) => {
+    const element = $(el);
+
+    element.on("click", () => {
+        $("[data-zoom-view]").removeClass("active");
+
+        const zoomView = $(element.data("zoom-view"));
+
+        zoomView.attr("src", element.data("image"));
+        element.addClass("active");
+    });
+});
