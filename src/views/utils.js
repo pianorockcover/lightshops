19f2b80a973cc41c0d1b97159d9a5ec7c6cba13b
65pnk <65pnk@cbr.ru>
2022-01-03 16:46:45 +0500

    prepare to redesign

--- src/views/utils.js
+++ src/views/utils.js
@@ -5,7 +5,10 @@ const faker = require("faker");
 const randomInt = (min, max) => Math.trunc(Math.random() * (max - min) + min);
 const randomElem = (items) => items[Math.floor(Math.random() * items.length)];
 
-const getUpload = (theme) => (path, forceTheme) => `images/uploads/${forceTheme ? forceTheme : theme}/${path}`;
+const getUpload = (theme) => (path, forceTheme) => {
+    // `images/uploads/${forceTheme ? forceTheme : theme}/${path}`
+    return `images/placeholder.svg`;
+};
 
 const randomColor = () => {
     const letters = '0123456789ABCDEF';
